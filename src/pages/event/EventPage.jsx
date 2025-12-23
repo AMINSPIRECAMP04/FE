ff5b4191282aeeacc633bc2a76f57cc6103b5f0f
Yeongeunn <nye0817@gmail.com>
2025-12-23 16:47:13 +0900

    feat: 사용자 로그인 입력 폼 및 양방향 데이터 바인딩 구현
    - 리액트 이벤트 핸들링 및 State 싱크 원리 실습
    - 이벤트 소스(input), 이벤트(onChange), 핸들러(idHandler) 구조 학습
    - e.target.value를 통한 상태 업데이트 구현

--- src/pages/event/EventPage.jsx
+++ src/pages/event/EventPage.jsx
@@ -1,15 +1,30 @@
 //import Button from "../../components/ui/Button";
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useState } from 'react'
 
 const EventPage = () => {
 
     const handler = (id, pwd) => {
         console.log('>>>>>> handler call', id, pwd)
     }
+    //이벤트 소스, 이벤트, 이벤트 핸들러가 있음(3가지)
 
-    const id = 'jslim';
-    const pwd = 'jslim';
+    const idHandler = (e) => { //이벤트를 받음
+        //setId(id); //이렇게 해도 안 됨
+        setId(e.target.value);
+    }
+
+    const pwdHandler = (e) => { //이벤트에 대한 동작을 객체로 전달받음
+        setPwd(e.target.value); //타켓은 인풋이되고 그 밸류를 가져오라고 함
+    }
+
+    
+    const [id, setId] = useState('');
+    const [pwd, setPwd] = useState('');
+
+
+    //const pwd = 'jslim';
 
     return(
 
@@ -17,14 +32,34 @@ const EventPage = () => {
             <div>
             <label>아이디</label>
             <input
-            typr="text"
+            type="text"
+            value={id} //input의 value로 아이디를 걸어준다.
+            //onChange={}//핸들러를 직접 호출할 수도 있고, 특정 값만 변경시킬 거라면 어떻게 할까?
+            // onChange={ () => {
+            //     setId(id);
+            // }} -> 이렇게 하면 안 됨
+
+            //onChange={idHandler} -> 이것도 부족함
+
+            onChange={(e) => idHandler(e)}
             placeholder='아이디를 입력하세요'/>
             </div>
-            <Button variant='primary' onClick = { () => handler()} >login</Button>
+            <div>
+            <label>비밀번호</label>
+            <input
+            type="text"
+ 
+            value={pwd}
+
+            onChange={(e) => pwdHandler(e)}
+            placeholder='비밀번호를 입력하세요'/>
+            </div>
+
+            <Button variant='primary' onClick = { () => handler(id, pwd)} >login</Button>
 
         </div>
         //리액트안에서 폼을 만든다면 리액트에 의해 제어가 되어야 한다. 입력된 값과 ~~가 싱크되어서 실시간으로 동기화가 되어야 한다.
-        //리턴 안에서 스크립트를 하기 위해선 중괄호쓰면 됨 {}
+        //리턴 안에서 스크립트를 하려면 중괄호쓰면 됨 {}
     )
 }
 
